5da13420d8c8dca4609eb5a34943bfc07c53f0a4
Cory Snyder <csnyder@iland.com>
2022-06-22 06:56:27 -0400

    intial commit

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::Arc;
+
+use async_trait::async_trait;
+
+pub type Stop = Box<dyn FnOnce() -> Pin<Box<dyn Future<Output=()> + Send>>>;
+
+#[async_trait]
+pub trait Lifecycle {
+    async fn start(self: Arc<Self>) -> Stop;
+}
+
+#[macro_export]
+macro_rules! interval_worker {
+    ($name:expr, $interval:expr, $body:expr) => {{
+        if cfg!(tracing) {
+            tracing::info!("{} starting", $name);
+        }
+        let (tx, mut rx) = tokio::sync::oneshot::channel();
+        let jh = tokio::spawn(async move {
+            let sleep = tokio::time::sleep($interval.to_std().unwrap());
+            tokio::pin!(sleep);
+            loop {
+                select! {
+                    _ = &mut sleep => {
+                        $body.await;
+                        sleep.as_mut().reset(tokio::time::Instant::now() + $interval.to_std().unwrap());
+                    },
+                    _ = &mut rx => {
+                        return;
+                    }
+                }
+            }
+        });
+        Box::new(move || {
+            Box::pin(async move {
+                if cfg!(tracing) {
+                    tracing::info!("{} stopping", $name);
+                }
+                let _ = tx.send(());
+                tokio::time::timeout(std::time::Duration::from_secs(60), jh)
+                    .await
+                    .unwrap()
+                    .unwrap();
+                if cfg!(tracing) {
+                    tracing::info!("{} stopped", $name);
+                }
+            })
+        })
+    }};
+}
+
+#[macro_export]
+macro_rules! no_teardown {
+    () => {
+        Box::new(|| Box::pin(async {}))
+    };
+}
