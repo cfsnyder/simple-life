51e988d1da75c3137e96d91252d5d2ed8582f661
Cory Snyder <csnyder@iland.com>
2022-07-22 05:43:39 -0400

    add os signal helper

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use std::task::{Context, Poll};
 use std::time::Duration;
 
 use async_trait::async_trait;
+use tokio::signal;
+use tokio::signal::unix::SignalKind;
+use tokio::time::error::Elapsed;
 
 #[async_trait]
 pub trait Stop: Send {
@@ -18,13 +21,11 @@ pub trait Lifecycle: Send + 'static {
 }
 
 pub fn seq<A, B>(a: A, b: B) -> impl Lifecycle
-    where
-        A: Lifecycle,
-        B: Lifecycle,
+where
+    A: Lifecycle,
+    B: Lifecycle,
 {
-    lifecycle!(state, {
-        (a.start().await, b.start().await)
-    }, {
+    lifecycle!(state, { (a.start().await, b.start().await) }, {
         let (a_stop, b_stop) = state;
         b_stop.stop().await;
         a_stop.stop().await;
@@ -32,13 +33,11 @@ pub fn seq<A, B>(a: A, b: B) -> impl Lifecycle
 }
 
 pub fn parallel<A, B>(a: A, b: B) -> impl Lifecycle
-    where
-        A: Lifecycle,
-        B: Lifecycle,
+where
+    A: Lifecycle,
+    B: Lifecycle,
 {
-    lifecycle!(state, {
-        tokio::join!(a.start(), b.start())
-    }, {
+    lifecycle!(state, { tokio::join!(a.start(), b.start()) }, {
         let (a_stop, b_stop) = state;
         let _ = tokio::join!(a_stop.stop(), b_stop.stop());
     })
@@ -62,43 +61,37 @@ macro_rules! seq {
 
 #[macro_export]
 macro_rules! lifecycle {
-    ($state:ident, $start:block, $stop:block) => (
-        move || {
-            async move {
-                let $state = $start;
-                move || {
-                    async move {
-                        $stop
-                    }
-                }
-            }
+    ($state:ident, $start:block, $stop:block) => {
+        move || async move {
+            let $state = $start;
+            move || async move { $stop }
         }
-    );
-    ($start:block, $stop:block) => (
+    };
+    ($start:block, $stop:block) => {
         simple_life::lifecycle!(_state, $start, $stop)
-    );
+    };
 }
 
 #[macro_export]
 macro_rules! start {
-    ($x:block) => (
+    ($x:block) => {
         simple_life::lifecycle!($x, {})
-    );
+    };
 }
 
 #[macro_export]
 macro_rules! stop {
-    ($x:block) => (
+    ($x:block) => {
         simple_life::lifecycle!({}, $x)
-    );
+    };
 }
 
 #[async_trait]
 impl<F, R, O> Lifecycle for F
-    where
-        F: FnOnce() -> R + 'static + Send,
-        R: Future<Output=O> + Send,
-        O: Stop,
+where
+    F: FnOnce() -> R + 'static + Send,
+    R: Future<Output = O> + Send,
+    O: Stop,
 {
     type S = O;
 
@@ -109,9 +102,9 @@ impl<F, R, O> Lifecycle for F
 
 #[async_trait]
 impl<F, R> Stop for F
-    where
-        F: FnOnce() -> R + Send,
-        R: Future<Output=()> + Send,
+where
+    F: FnOnce() -> R + Send,
+    R: Future<Output = ()> + Send,
 {
     async fn stop(self) {
         self().await
@@ -119,24 +112,24 @@ impl<F, R> Stop for F
 }
 
 pub fn spawn_interval<S, F, R>(s: S, period: Duration, fun: F) -> impl Lifecycle
-    where
-        S: Clone + 'static + Send + Sync,
-        F: Fn(S) -> R + Send + Sync + 'static,
-        R: Future<Output=()> + Send,
+where
+    S: Clone + 'static + Send + Sync,
+    F: Fn(S) -> R + Send + Sync + 'static,
+    R: Future<Output = ()> + Send,
 {
     spawn_with_shutdown(move |mut sig| async move {
         let sleep = tokio::time::sleep(period);
         tokio::pin!(sleep);
         loop {
             tokio::select! {
-                    _ = &mut sleep => {
-                        fun(s.clone()).await;
-                        sleep.as_mut().reset(tokio::time::Instant::now() + period);
-                    },
-                    _ = &mut sig => {
-                        return;
-                    }
+                _ = &mut sleep => {
+                    fun(s.clone()).await;
+                    sleep.as_mut().reset(tokio::time::Instant::now() + period);
+                },
+                _ = &mut sig => {
+                    return;
                 }
+            }
         }
     })
 }
@@ -152,21 +145,42 @@ impl Future for ShutdownSignal {
 }
 
 pub fn spawn_with_shutdown<F, R>(fun: F) -> impl Lifecycle
-    where
-        F: FnOnce(ShutdownSignal) -> R + Send + 'static,
-        R: Future<Output=()> + Send,
+where
+    F: FnOnce(ShutdownSignal) -> R + Send + 'static,
+    R: Future<Output = ()> + Send,
 {
-    lifecycle!(chans, {
-        let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
-        let jh = tokio::spawn(async move {
-             let _ = fun(ShutdownSignal(shutdown_rx)).await;
-        });
-        (shutdown_tx, jh)
-    }, {
-        let (shutdown_tx, jh) = chans;
-        shutdown_tx.send(()).unwrap();
-        let _ = jh.await;
-    })
+    lifecycle!(
+        chans,
+        {
+            let (shutdown_tx, shutdown_rx) = tokio::sync::oneshot::channel::<()>();
+            let jh = tokio::spawn(async move {
+                let _ = fun(ShutdownSignal(shutdown_rx)).await;
+            });
+            (shutdown_tx, jh)
+        },
+        {
+            let (shutdown_tx, jh) = chans;
+            shutdown_tx.send(()).unwrap();
+            let _ = jh.await;
+        }
+    )
+}
+
+pub async fn run_until_shutdown_sig(
+    life: impl Lifecycle,
+    timeout: Duration,
+) -> Result<(), Elapsed> {
+    let stopper = life.start().await;
+    std_unix_shutdown_sigs().await;
+    tokio::time::timeout(timeout, stopper.stop()).await
+}
+
+async fn std_unix_shutdown_sigs() {
+    let mut kill_sig = signal::unix::signal(SignalKind::terminate()).unwrap();
+    tokio::select! {
+        _ = signal::ctrl_c() => {},
+        _ = kill_sig.recv() => {},
+    }
 }
 
 #[derive(Eq, PartialEq, Debug)]
